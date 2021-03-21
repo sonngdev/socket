75c6db381d1e507116e749cc153c03ac513f7db8
Nguyễn Thanh Sơn <son@gotitapp.co>
2021-03-21 18:08:52 +0700

    feat(v2): expose /message and /disconnect endpoints

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const http = require('http');
 const express = require('express');
 const socketIo = require('socket.io');
 const config = require('./config');
+const PubSub = require('./PubSub');
+const TutorList = require('./TutorList');
 
 const conf = config(process.env.NODE_ENV);
 const rest = express();
@@ -13,6 +15,21 @@ const socket = socketIo(socketServer, {
     methods: ['GET', 'POST'],
   },
 });
+const pubsub = new PubSub();
+const tutorList = new TutorList();
+
+/**
+|--------------------------------------------------
+| Helpers
+|--------------------------------------------------
+*/
+// function login(uid) {
+//   console.log(`Log in user with uid ${uid}`);
+// }
+
+// function logout(uid) {
+//   console.log(`Log out user with uid ${uid}`);
+// }
 
 /**
 |--------------------------------------------------
@@ -25,14 +42,78 @@ rest.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
+rest.post('/message', (req, res) => {
+  const { uids, data } = req.body;
+  if (!Array.isArray(uids)) {
+    res.status(400).json('uids is invalid, expected an array');
+    return;
+  }
+  if (uids.length === 0) {
+    pubsub.broadcast('message', data);
+    res.json(uids);
+  } else {
+    const published = [];
+    uids.forEach((uid) => {
+      if (tutorList.hasTutor(uid) && pubsub.publish(uid, 'message', data)) {
+        published.push(uid);
+      }
+    });
+    res.json(published);
+  }
+});
+
+rest.post('/disconnect', (req, res) => {
+  const { uid, data } = req.body;
+  if (!tutorList.hasTutor(uid)) {
+    res.status(404).json('Tutor not found');
+    return;
+  }
+  if (pubsub.publish(uid, 'disconnect', data)) {
+    res.json(uid);
+  } else {
+    res.status(500).json('Server error');
+  }
+});
+
 /**
 |--------------------------------------------------
 | Socket app
 |--------------------------------------------------
 */
+socket.use((connection, next) => {
+  const { uid, name, avatar } = connection.request._query;
+  console.log(`Socket connection request from uid ${uid}, name ${name}, avatar ${avatar}`);
+  next();
+});
+
 socket.on('connection', (connection) => {
   const { uid, name, avatar } = connection.handshake.query;
+  tutorList.addTutor(uid, name, avatar);
+
+  connection.on('disconnect', (reason) => {
+    tutorList.removeTutor(uid);
+  });
+
+  pubsub.subscribe(uid);
+
+  pubsub.onBroadcastMessage('message', (data) => {
+    socket.sockets.emit('message', data);
+  });
+
+  pubsub.onMessage(uid, 'message', (data) => {
+    connection.emit('message', data);
+  });
+
+  pubsub.onMessage(uid, 'disconnect', (data) => {
+    if (tutorList.hasTutor(uid) && connection.emit('message', data)) {
+      tutorList.removeTutor(uid);
+      pubsub.unsubscribe(uid);
+      connection.disconnect(true);
+    }
+  });
 });
+
+pubsub.subscribeBroadcast();
 
 /**
 |--------------------------------------------------
